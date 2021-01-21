91ba4f95f6c2d03d6370e7627115fc40a4330e9c
Michel Lorry <74318557+MLorry@users.noreply.github.com>
2021-01-21 15:51:56 +0100

    Ennemy+book

---
+++ pj unity/VR Horror Game test/Assets/SCRIPTS/Ennemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    private int health = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Book")
+        {   
+
+            StartCoroutine(GetDamage());
+        }
+    }
+
+    IEnumerator GetDamage()
+    {
+        health -= 1;
+        GetComponent<Renderer>().material.color = new Color(1, 0, 1, 1); //C#  }
+        yield return new WaitForSeconds(0.2f);
+        GetComponent<Renderer>().material.color = new Color(0, 1, 1, 1); //C#  }
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this);
+    }    
+}
